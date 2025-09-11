e902c5a0c0523b8a92a26bb5a093d6b1e87cff59
Annas Khan <annasking601@gmail.com>
2025-09-11 13:36:04 +0500

    Changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
       </nav>
 
       {/* Bottom Navigation */}
-      <div className="fixed bottom-4 sm:bottom-5 left-1/2 transform -translate-x-1/2 z-50 w-full px-2 sm:px-0">
+      <div className="fixed bottom-4 sm:bottom-5 left-1/2 transform -translate-x-1/2 z-50 w-full px-0 sm:px-0">
         <ul className="flex justify-center flex-wrap gap-2 sm:gap-4 bg-[#0A0A0A] backdrop-blur-md px-4 sm:px-8 py-2 sm:py-3 rounded-full text-xs sm:text-sm md:text-base font-medium border border-[#171717] max-w-max mx-auto">
           <li className="hover:text-[#000000] cursor-pointer uppercase font-light transition bg-[#E7E7E9] px-3 sm:px-4 py-1.5 sm:py-2 rounded-full">
             Home
